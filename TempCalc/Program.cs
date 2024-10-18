a0272b2ab19ac0b115c75343d4f8ac6f768f6b3d
Andre K. <Andre.kukk@tktk.ee>
2024-10-18 14:25:32 +0300

    first commit

---
+++ TempCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Vali teisendamise tüüp (1/2)");
+Console.WriteLine("1 on F to C ja 2 on C to F");
+
+int choice = Int32.Parse(Console.ReadLine());
+
+if (choice == 1)
+{
+    Console.WriteLine("Sisesta temperatuur Fahrenheitis:");
+    double Fahrenheit = Double.Parse(Console.ReadLine());
+    ConvertToCelcius(Fahrenheit);
+}
+else if (choice == 2)
+{
+    Console.WriteLine("Sisesta temperatuur Celsiuses:");
+    double celsius = Double.Parse(Console.ReadLine());
+    ConvertToFahrenheit(celsius);
+}
+else
+{
+    Console.WriteLine("Vale valik");
+}
+
+static void ConvertToCelcius(double fahrenheit)
+{
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    Console.WriteLine($"{fahrenheit}°F on {celsius:F2}°C.");
+}
+
+static void ConvertToFahrenheit(double celsius)
+{
+    double fahrenheit = (celsius * 9) / 5 + 32;
+    Console.WriteLine($"{celsius}°C on {fahrenheit:F2}°F.");
+}
---
+++ Ül 6. Temp calc/Program.cs
@@ -0,0 +1,42 @@
+﻿    static void Main()
+    {
+        Console.WriteLine("Vali teisendamise tüüp:");
+        Console.WriteLine("1: Fahrenheit to Celsius");
+        Console.WriteLine("2: Celsius to Fahrenheit");
+
+   
+        int choice = Int32.Parse(Console.ReadLine());
+
+       
+        if (choice == 1)
+        {
+            Console.WriteLine("Sisesta temperatuur Fahrenheitis:");
+            double fahrenheit = Double.Parse(Console.ReadLine());
+            ConvertToCelsius(fahrenheit);
+        }
+        else if (choice == 2)
+        {
+            Console.WriteLine("Sisesta temperatuur Celsiuses:");
+            double celsius = Double.Parse(Console.ReadLine());
+            ConvertToFahrenheit(celsius);
+        }
+        else
+        {
+            Console.WriteLine("Vale valik. Palun vali kas 1 või 2.");
+        }
+    }
+
+    // Funktsioon Fahrenheitist Celsiuse arvutamiseks
+    static void ConvertToCelsius(double fahrenheit)
+    {
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine($"{fahrenheit} kraadi Fahrenheit on {celsius} kraadi Celsiust.");
+    }
+
+    // Funktsioon Celsiuse teisendamiseks Fahrenheitiks
+    static void ConvertToFahrenheit(double celsius)
+    {
+        double fahrenheit = (celsius * 9) / 5 + 32;
+        Console.WriteLine($"{celsius} kraadi Celsiust on {fahrenheit} kraadi Fahrenheiti.");
+    }
+
